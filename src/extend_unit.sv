9ff287e978a30a4857d6e20eddfae4917eb96928
Platty114 <jamesplatt02@gmail.com>
2024-11-16 12:57:47 -0700

    update extend_unit and update testbench

--- src/extend_unit.sv
+++ src/extend_unit.sv
@@ -3,7 +3,7 @@
 
 
 module extend_unit(
-    input logic [24:0] src,
+    input logic [31:7] src,
     input logic [1:0] control,
     output logic [31:0] extended_src
 );
@@ -12,13 +12,12 @@ module extend_unit(
   always_comb begin
 
       case(control)
-          
-          2'b00: extended_src = {{20{src[24]}}, src[24:13]}; // I type
-          2'b01: extended_src = {{20{src[24]}}, src[24:18], src[4:0]}; // S type
-          2'b10: extended_src = {{20{src[24]}}, src[0], src[23:18], src[4:1], 1'b0}; // B type
-          2'b11: extended_src = {{12{src[24]}}, src[12:5], src[13], src[23:14], 1'b0}; // J type
+           
+          2'b00: extended_src = {{21{src[31]}}, src[30:20]}; // I type
+          2'b01: extended_src = {{21{src[31]}}, src[30:25], src[11:7]}; // S type
+          2'b10: extended_src = {{20{src[31]}}, src[7], src[30:25], src[11:8], 1'b0}; // B type
+          2'b11: extended_src = {{12{src[31]}}, src[19:12], src[20], src[30:21], 1'b0}; // J type 
           default: extended_src = 32'bx;
-
       endcase
 
   end
--- src/extend_unit_tb.sv
+++ src/extend_unit_tb.sv
@@ -18,7 +18,7 @@ module extend_unit_tb();
     extend_unit uut(
         .src(instruction[31:7]),
         .control(control),
-        .extend_src(immediate)
+        .extended_src(immediate)
     );
 
     always begin
@@ -27,9 +27,115 @@ module extend_unit_tb();
 
     initial begin
 
+        //initialize
+        instruction = 32'h0000_0000;
+        control = 2'b00;
+        #(period);
          
+        //addi x1, x0, 6, I Type
+        //32'b00000000011000000000000010010011
+        instruction = 32'b00000000011000000000000010010011;
+        control = 2'b00;
+        #(period);
+
+        if(immediate != 6) begin
+            $display("Incorrect immediate for I type, value 6");
+            error = 1'b1;
+            #(period);
+            error = 1'b0;
+        end 
+
+        //addi x1, x0, -6, I Type
+        //32'b11111111101000000000000010010011
+        instruction = 32'b11111111101000000000000010010011;
+        control = 2'b00;
+        #(period);
+
+        if(immediate != -6) begin
+            $display("Incorrect immediate for I type, value -6");
+            error = 1'b1;
+            #(period);
+            error = 1'b0;
+        end 
+         
+        //sw x1, 6(x2), S Type
+        //32'b00000000001000001010001100100011
+        instruction = 32'b00000000001000001010001100100011;
+        control = 2'b01;
+        #(period);
+
+        if(immediate != 6) begin
+            $display("Incorrect immediate for S type, value 6");
+            error = 1'b1;
+            #(period);
+            error = 1'b0;
+        end
+
+        //sw x1, -6(x2), S Type
+        //32'b00001100001000001010001100100011
+        instruction = 32'b11111110001000001010110100100011;
+        control = 2'b01;
+        #(period);
+
+        if(immediate != -6) begin
+            $display("Incorrect immediate for S type, value -6");
+            error = 1'b1;
+            #(period);
+            error = 1'b0;
+        end
+
+        //beq x1, x2, 4, B Type
+        //32'b00000000001000001000001001100011
+        instruction = 32'b00000000001000001000001001100011;
+        control = 2'b10;
+        #(period);
+
+        if(immediate != 4) begin
+            $display("Incorrect immediate for B type, value 4");
+            error = 1'b1;
+            #(period);
+            error = 1'b0;
+        end
+
+        //beq x1, x2, -4, B Type
+        //32'b11111000001000001000001001100011
+        instruction = 32'b11111110001000001000111011100011;
+        control = 2'b10;
+        #(period);
+
+        if(immediate != -4) begin
+            $display("Incorrect immediate for B type, value -4");
+            error = 1'b1;
+            #(period);
+            error = 1'b0;
+        end
+
+        //jal x9, 26, J Type
+        instruction = 32'b00000001101000000000010011101111;
+        control = 2'b11;
+        #(period);
+
+        if(immediate != 26) begin
+            $display("Incorrect immediate for J type, value 25");
+            error = 1'b1;
+            #(period);
+            error = 1'b0;
+        end
+
+        //jal x9, -8, J Type
+        instruction = 32'b11111111100111111111010011101111;
+        control = 2'b11;
+        #(period);
+
+        if(immediate != -8) begin
+            $display("Incorrect immediate for J type, value -8");
+            error = 1'b1;
+            #(period);
+            error = 1'b0;
+        end
 
 
+        $stop;
 
     end 
 endmodule
